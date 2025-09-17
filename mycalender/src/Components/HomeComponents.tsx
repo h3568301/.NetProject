0c20829355fabdadc107e5427ce477cb12811bc2
h3568301 <anson940@connect.hku.hk>
2025-09-17 14:47:01 +0100

    frontend update

--- mycalender/src/Components/HomeComponents.tsx
+++ mycalender/src/Components/HomeComponents.tsx
@@ -61,6 +61,7 @@ const HomeComponents = (props: Props) => {
                             eventTimeFormat={{
                                 hour: '2-digit',
                                 minute: '2-digit',
+                                second: '2-digit',
                                 hour12: true
                             }}
                         />
